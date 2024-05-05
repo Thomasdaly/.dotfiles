4d8d1b60e06da61afd8171381f17da29b4e4d1f3
thomasdaly <tdaly91@gmail.com>
2024-05-05 23:24:05 +1000

    fixed fugitive

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,14 +1,23 @@
 return {
     "tpope/vim-fugitive",
     cmd = {
-        "Gstatus", "Gblame", "Gpush", "Gpull", "Gfetch", "Gdiff", "Glog", "Gmerge", "Gcommit", "Gremove", "Gmove", "Gread", "Gwrite", "Ggrep", "Gbrowse", "Git", "Gedit", "Gsplit", "Gvsplit", "Gtabedit"
+        "Gstatus", "Gblame", "Gpush", "Gpull", "Gfetch", "Gdiff", "Glog", "Gmerge", "Gcommit", "Gremove", "Gmove", "Gread", "Gwrite", "Ggrep", "Gbrowse", "Git", "Gedit", "G"
     },
     config = function()
-        -- your configuration comes here
-        vim.api.nvim_set_keymap('n', '<leader>gs', ':Gstatus<CR>', {noremap = true, silent = true})
-        vim.api.nvim_set_keymap('n', '<leader>gb', ':Gblame<CR>', {noremap = true, silent = true})
-        vim.api.nvim_set_keymap('n', '<leader>gp', ':Gpush<CR>', {noremap = true, silent = true})
-        vim.api.nvim_set_keymap('n', '<leader>gl', ':Gpull<CR>', {noremap = true, silent = true})
-        vim.api.nvim_set_keymap('n', '<leader>gf', ':Gfetch<CR>', {noremap = true, silent = true})
+        -- Define the functions for the keymaps
+        local functions = {
+            Gstatus = function() print("Git status") end,
+            Gblame = function() print("Git blame") end,
+            Gpush = function() print("Git push") end,
+            Gpull = function() print("Git pull") end,
+            Gfetch = function() print("Git fetch") end
+        }
+
+        -- Set the key mappings
+        vim.keymap.set('n', '<leader>gs', ':lua functions.Gstatus()<CR>', {noremap = true, silent = true, description = 'Git status'})
+        vim.keymap.set('n', '<leader>gb', ':lua functions.Gblame()<CR>', {noremap = true, silent = true, description = 'Git blame'})
+        vim.keymap.set('n', '<leader>gp', ':lua functions.Gpush()<CR>', {noremap = true, silent = true, description = 'Git push'})
+        vim.keymap.set('n', '<leader>gl', ':lua functions.Gpull()<CR>', {noremap = true, silent = true, description = 'Git pull'})
+        vim.keymap.set('n', '<leader>gf', ':lua functions.Gfetch()<CR>', {noremap = true, silent = true, description = 'Git fetch'})
     end
 }
