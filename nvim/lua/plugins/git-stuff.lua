a4ed2847fc26e6a1948a92d07cbdcd8ccc1439f8
thomasdaly <tdaly91@gmail.com>
2024-05-06 19:28:46 +1000

    Fixed somekeymaps

--- nvim/lua/plugins/git-stuff.lua
+++ nvim/lua/plugins/git-stuff.lua
@@ -1,14 +1,19 @@
 return {
   {
-    "tpope/vim-fugitive"
+    "tpope/vim-fugitive",
+        config = function ()
+            vim.keymap.set("n", "<leader>gs", ":Git<CR>", {desc = "Show git status"})
+            vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", {desc = "Initiate git commit"})
+            vim.keymap.set("n", "<leader>gp", ":Git pull<CR>", {desc = "Initiate git pull"})
+            vim.keymap.set("n", "<leader>gP", ":Git push<CR>", {desc = "Initiate git push"})
+        end
   },
   {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
-
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+      vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {desc = "Preview git hunk"})
+      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {desc = "Toggle blame for current line"})
     end
   }
 }
--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,9 +1,9 @@
 return {
     'echasnovski/mini.nvim', version = false,
     config = function()
-        require('mini.bracketed').setup()
-        require('mini.ai').setup()
-        require('mini.comment').setup()
-        require('mini.surround').setup()
+        require('mini.bracketed').setup({})
+        require('mini.ai').setup({})
+        require('mini.comment').setup({})
+        require('mini.surround').setup({})
     end
 }
